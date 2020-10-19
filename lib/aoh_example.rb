bacb835dc77947e0236eac844d41cedc7c8013eb
Sara Cemal <saracemal@gmail.com>
2020-10-19 03:46:18 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -19,6 +19,8 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
+  aoh = [
+    ]
 end
 
 def aoh_lookup(aoh, row, key)
