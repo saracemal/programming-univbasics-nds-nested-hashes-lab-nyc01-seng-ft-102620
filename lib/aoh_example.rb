b2ea8c6ce19af10894407abf702916fbc9a9bfa0
Sara Cemal <saracemal@gmail.com>
2020-10-19 03:40:28 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -7,9 +7,8 @@ KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
-  
   aoh = [
-    DON_G = {name: "Don Gately", occupation: "Live-in Staff"},
+  DON_G = {name: "Don Gately", occupation: "Live-in Staff"},
   JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" },
   PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" },
   KATE_G = { name:  "Kate Gompert", occupation: "None" },
